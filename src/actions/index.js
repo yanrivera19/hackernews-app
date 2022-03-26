f5507b4ca428f4fa0d9453a6901d5645a9bf9a1d
yanrivera19 <yan.rivera.6@gmail.com>
2022-03-26 12:08:54 -0700

    Added error handling to action creators that make requests to the api.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,31 +1,39 @@
 import news from "../apis/news";
 import { FETCH_NEWS, FETCH_TOP_NEWS, SET_PAGE_NUMBER, SET_TERM } from "./types";
 
-export const fetchNews = 
+export const fetchNews =
 	(term = "cybersecurity") =>
 	async (dispatch) => {
-		const response = await news.get("/everything", {
-			params: {
-				q: term,
-				pageSize: 100,
-				language: "en",
-				sortBy: "publishedAt",
-			},
-		});
+		try {
+			const response = await news.get("/everything", {
+				params: {
+					q: term,
+					pageSize: 100,
+					language: "en",
+					sortBy: "publishedAt",
+				},
+			});
 
-		dispatch({ type: FETCH_NEWS, payload: response.data.articles });
+			dispatch({ type: FETCH_NEWS, payload: response.data.articles });
+		} catch (error) {
+			console.log(error.response);
+		}
 	};
 
 export const fetchTopNews = (term) => async (dispatch) => {
-	const response = await news.get("/top-headlines", {
-		params: {
-			category: term,
-			pageSize: 9,
-			language: "en",
-		},
-	});
+	try {
+		const response = await news.get("/top-headlines", {
+			params: {
+				category: term,
+				pageSize: 9,
+				language: "en",
+			},
+		});
 
-	dispatch({ type: FETCH_TOP_NEWS, payload: response.data.articles });
+		dispatch({ type: FETCH_TOP_NEWS, payload: response.data.articles });
+	} catch (error) {
+		console.log(error.response);
+	}
 };
 
 export const setPageNumber = (pageNumber) => {
